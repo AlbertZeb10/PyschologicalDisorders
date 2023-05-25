92010ff9cb2399f82024498bbcb47b6640901a77
AlbertZeb10 <108046490+AlbertZeb10@users.noreply.github.com>
2023-05-25 13:50:28 +0700

    PyschologicalDisorders

---
+++ modul/consultant/consultant.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+  <title>Consultant Pyschlogical Disorders</title>
+
+  <style>    
+
+	.profile-card header img{
+		overflow: hidden;  
+		width: 150px;
+		/*border: 1px solid rgba(255,255,255,.5);*/
+		box-shadow: 0 1px 0 rgba(0,0,0,.1),0 1px 2px rgba(0,0,0,.1);
+	  }
+
+    .card1:hover {
+      background:#00ffb6;
+      border:1px solid #00ffb6;
+    }
+
+    .card1:hover .list-group-item{
+      background:#00ffb6 !important
+    }
+
+    .card2:hover {
+      background:#00C9FF;
+      border:1px solid #00C9FF;
+    }
+
+    .card2:hover .list-group-item{
+      background:#00C9FF !important
+    }
+
+
+    .card3:hover {
+      background:#ff95e9;
+      border:1px solid #ff95e9;
+    }
+
+    .card3:hover .list-group-item{
+      background:#ff95e9 !important
+    }
+
+
+    .card:hover .btn-outline-dark{
+      color:white;
+      background:#212529;
+    }
+
+  </style>
+</head>
+<body>
+
+
+  <div class="container-fluid">
+    <div class="container p-5">
+      <div class="row">
+        <div class="col-lg-4 col-md-12 mb-4">
+          <div class="card card1 h-100">
+            <div class="card-body">
+
+            <aside class="profile-card">
+      <header>
+
+    <!-- avatar -->
+      <img src="gambar/albert.jpg">
+      </header>
+  </aside>
+              <h5 class="card-title">ALBERT SAPUTRA ZEBUA S.KOMP, M.PSI</h5>
+              <small class='text-muted'>Psikolog Klinis, Gangguan Mood, Depresi & Stress</small>
+              <br><br>
+              <span class="h2">$100</span>/month
+              <br><br>
+              <div class="d-grid my-3">
+                <button class="btn btn-outline-dark btn-block">Chat</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-12 mb-4">
+          <div class="card card2 h-100">
+            <div class="card-body">
+            <aside class="profile-card">
+      <header>
+
+    <!-- avatar -->
+      <img src="gambar/jonatan.jpg">
+      </header>
+  </aside>
+             
+              <h5 class="card-title">JONATAN SIANTURI S.KOMP, M.PSI</h5>
+              <small class='text-muted'>Psikolog Klinis Identitas Sexual, Psikolog Keluarga</small>
+              <br><br>
+              <span class="h2">$100</span>/month
+              <br><br>
+              <div class="d-grid my-3">
+                <button class="btn btn-outline-dark btn-block">Chat</button>
+              </div>
+            </div>    
+          </div>
+        </div>
+
+
+        <div class="col-lg-4 col-md-12 mb-4">
+          <div class="card card3 h-100">
+            <div class="card-body">
+            <aside class="profile-card">
+      <header>
+
+    <!-- avatar -->
+      <img src="gambar/calvin.jpg">
+      </header>
+  </aside>
+             
+              <h5 class="card-title">CALVIN JEDDY NUGRAHA S.KOMP, M.PSI</h5>
+              <small class='text-muted'>Psikolog Klinis Anak & Dewasa</small>
+              <br><br>
+              <span class="h2">$100</span>/month
+              <br><br>
+              <div class="d-grid my-3">
+                <button class="btn btn-outline-dark btn-block">Chat</button>
+              </div>
+            </div>        
+          </div>
+        </div>
+      </div>    
+    </div>
+  </body>
+
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
---
+++ modul/tentang.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+   <title>Tentang - Psychlogical Disorders</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet prefetch' href='aset/tentang/style.css'>
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'>
+   <link rel="stylesheet" href="aset/login/css/style.css">
+  
+  
+</head>
+<style>
+@media only screen and (max-width: 400px) {
+    img {
+        width: 100%;
+    }
+}
+</style>
+<body>
+
+  <div class="ma3">
+  <article class="tc w-75 center pt5 pb2 ph3 mw6-ns ba bw1 b--light-gray" style="background: #fff;">
+    <header class="mb4">
+      <h1 class="f3 lh-title mv2 dark-gray">Psychlogical Disorders</h1>
+	  <p class="f6 silver mt2 mb0"><a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Teams 1"><i class="fa fa-user" aria-hidden="true"></i> Albert Saputra Zebua </button></a>
+<a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Teams 2"><i class="fa fa-user" aria-hidden="true"></i> Jonatan Sianturi </button></a>
+	 <a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Teams 3"><i class="fa fa-user" aria-hidden="true"></i> Calvin Jeddy Nugraha </button></a></p>
+	   <br>
+	   <h2 class="f5 silver mt2 mb1">AI Diagnosa Psychlogical Disorders</h2>
+	  <h2 class="f5 silver mt2 mb1"> Universitas Kristen Krida Wacana - Informatika 2020 <br> ©2023</h2>
+	  <br>
+    </header>
+    <p class="f6 tl lh-copy silver" style="margin: 20px;">
+      AI yang mampu mendiagnosa penyakit Psychglogical Disorders berdasarkan pengetahuan yang 
+      diberikan langsung dari Mahasiswa Informatika Angkatan 2020 Dari Kampus Universitas Kristen Krida Wacana 
+      melalui beberapa Jurnal. Penelitian ini menggunakan Metode dari Certainty Factor yang dimana berfungsi
+      untuk menghitung tingkat kepakaran dalam Psychglogical Disorders. Data penelitian ini terdiri dari Data 
+      Gejala & Data Penyakit pada Psychlogical Disorders beserta Data Solusi nya.
+      <br><br><br>
+      TEAMS:
+      <br>
+      1. Albert Saputra Zebua - 412020012
+      <br>
+      2. Jonatan Sianturi - 412020027
+      <br>
+      3. Calvin Jeddy Nugraha - 412020030
+    </p>
+  </article>
+</div>
+</body>
+
+</html>
